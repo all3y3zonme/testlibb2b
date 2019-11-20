d137e70bb32f793599d92ae45bc1295f04cdfa71
Ruslan <iwantletitdie@mail.ru>
2019-11-20 16:32:34 +0300

    add lint

--- lib/app.ts
+++ lib/app.ts
@@ -1,13 +1,12 @@
 import * as got from 'got';
+
 const tough = require('tough-cookie');
-const Cookie = tough.Cookie;
 
-
-
+const { Cookie } = tough;
 
 interface ParamsAddTask {
     readonly element_type: number;
-    readonly complete_till: DataCue;
+    readonly complete_till: number;
     readonly task_type: number;
     readonly text: string;
     readonly responsible_user_id: number;
@@ -21,81 +20,85 @@ interface ParamsUpdateTask {
 }
 export default class CrmApi {
     public login: string;
+
     public hash: string;
+
     public url: string;
+
     public strForm: string;
+
     public addAndUpdateTaskUrl: string;
+
     public cookie: string;
+
     public authUrl: string;
+
     constructor(login: string, hash: string, url: string) {
-        this.url = url;
-        this.strForm = JSON.stringify({
-            USER_LOGIN: login,
-            USER_HASH: hash
-        });
-        this.addAndUpdateTaskUrl = `${url}/api/v2/tasks`;
-        this.authUrl = `${url}/private/api/auth.php`;
+      this.url = url;
+      this.strForm = JSON.stringify({
+        USER_LOGIN: login,
+        USER_HASH: hash,
+      });
+      this.addAndUpdateTaskUrl = `${url}/api/v2/tasks`;
+      this.authUrl = `${url}/private/api/auth.php`;
     }
 
     public async connect() {
-        try {
-            let data: any = await got.post(this.authUrl, {
-                body: this.strForm
-            })
-            this.getCookie(data)
-        } catch (e) {
-            console.log(e);
-        }
-
+      try {
+        const data: any = await got.post(this.authUrl, {
+          body: this.strForm,
+        });
+        this.setCookie(data);
+      } catch (e) {
+        console.log(e);
+      }
     }
-    public getCookie(data: any) {
-        this.cookie = Cookie.parse(data.headers['set-cookie'][0]);
-        // this.cookie = ([data.headers['set-cookie'][0],data.headers['set-cookie'][1]]).join(',');
+
+    public setCookie(data: any) {
+      this.cookie = Cookie.parse(data.headers['set-cookie'][0]);
+      // this.cookie = ([data.headers['set-cookie'][0],data.headers['set-cookie'][1]]).join(',');
     }
 
     public async addTask(task: ParamsAddTask) {
-
-        let strTask = JSON.stringify({
-            "add": [task]
+      const strTask = JSON.stringify({
+        add: [task],
+      });
+      try {
+        const status = await got.post(this.addAndUpdateTaskUrl, {
+          headers: {
+            'Content-Type': 'application/hal+json',
+            'Runtime-Timestamp': '1508320306',
+            cookie: this.cookie,
+          },
+          body: strTask,
         });
-        try {
-            let status = await got.post(this.addAndUpdateTaskUrl, {
-                headers: {
-                    'Content-Type': 'application/hal+json',
-                    'Runtime-Timestamp': '1508320306',
-                    cookie: this.cookie
-                },
-                body: strTask
-            })
-            status.statusCode === 200 ? console.log('done! addTask') : console.log(status.statusCode);
-        } catch (e) {
-            if (e.statusCode === 401) {
-                console.log('Unauthorized!');
-            } else {
-                console.log('Params error');
-            }
+        status.statusCode === 200 ? console.log('done! addTask') : console.log(status.statusCode);
+      } catch (e) {
+        if (e.statusCode === 401) {
+          console.log('Unauthorized!');
+        } else {
+          console.log('Params error');
         }
+      }
     }
 
 
     public async updateTask(task: ParamsUpdateTask) {
-
-        let strTask: string = JSON.stringify({
-            "update": [task]
+      const strTask: string = JSON.stringify({
+        update: [task],
+      });
+      try {
+        const status = await got.post(this.addAndUpdateTaskUrl, {
+          headers: {
+            'Content-Type': 'application/hal+json',
+            'Runtime-Timestamp': '1508320306',
+            cookie: this.cookie,
+          },
+          body: strTask,
         });
-        try {
-            let status = await got.post(this.addAndUpdateTaskUrl, {
-                headers: {
-                    'Content-Type': 'application/hal+json',
-                    'Runtime-Timestamp': '1508320306',
-                    cookie: this.cookie
-                },
-                body: strTask
-            })
-            status.statusCode === 200 ? console.log('done! updateTask') : console.log(status.statusCode);
-        } catch (e) {
-            console.log('Status code - ', e.statusCode);
-        }
+        status.statusCode === 200 ? console.log('done! updateTask') : console.log(status.statusCode);
+      } catch (e) {
+        console.log('Status code - ', e.statusCode);
+      }
     }
 }
-
